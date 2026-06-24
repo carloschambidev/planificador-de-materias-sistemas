33911f5cf56e225afd74e4120861e563ded02bd5
carloschambiutndev <cchambi@frba.utn.edu.ar>
2026-06-23 21:58:27 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/planificador-de-materias-sistemas/',
 })
